0b43b816e6af4d2c46157098a17a481fc72add31
Xbodwf <xbodwf@gmail.com>
2025-08-26 13:55:37 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import pathData from './src/pathdata'
+import Parser from './src/parser'
+import Level,* as Structure from './src/structure'
+import * as Presets from './src/presets'
+
+export {
+    pathData,
+    Parser,
+    Level,
+    Structure,
+    Presets
+}
---
+++ src/structure.ts
@@ -0,0 +1,6 @@
+import { Level } from './structure/Level';
+import { AdofaiEvent, LevelOptions, EventCallback, GuidCallback, Tile, ParseProvider } from './structure/interfaces';
+
+export { AdofaiEvent, LevelOptions, EventCallback, GuidCallback, Tile, ParseProvider };
+
+export default Level;
